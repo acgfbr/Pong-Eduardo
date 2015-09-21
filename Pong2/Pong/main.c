941ce9efd2c6f13ee764afe66308cde88ef8edaa
tonepanetone <dh.18@msn.com>
2015-09-21 18:18:06 -0300

    menu feito.
    menu finalizado.

---
+++ Pong2/Pong/main.c
@@ -0,0 +1,77 @@
+#include "pong.h"
+
+void Play();
+
+int main(int argc, char* argv[])
+{
+	// START DO SDL #####################################
+	const string title = "Pong by Elite Game Dev - 21/09/2015";
+	SDL_Init(SDL_INIT_EVERYTHING);
+	Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 2048);
+	SDL_Window * main_window = SDL_CreateWindow(title, SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, main_window_x, main_window_y, SDL_WINDOW_SHOWN);
+
+	if (main_window != NULL)
+	{
+		main_renderer = SDL_CreateRenderer(main_window, -1, 0);
+	}
+	// START DO SDL #####################################
+
+	// LOAD IMAGENS
+	menu = SDL_LoadBMP("menu.bmp");
+	play = SDL_LoadBMP("Play.bmp");
+	// LOAD IMAGENS
+	int Menu = 1;
+
+	while (Menu)
+	{
+		SDL_Event event;
+		tela = SDL_GetWindowSurface(main_window);
+		while (SDL_PollEvent(&event))
+		{
+
+			SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, 255); // seta preto como plano de fundo.
+			SDL_RenderClear(main_renderer); // limpa o render [ janela atual ]
+
+			if ((event.button.x >= 601 && event.button.y >= 69) && (event.button.x <= 688 && event.button.y <= 94)) // START
+			{
+				SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, 255);
+				SDL_BlitSurface(play, NULL, tela, NULL);
+				SDL_UpdateWindowSurface(main_window);
+			}
+			else if ((event.button.x >= 601 && event.button.y >= 149) && (event.button.x <= 679 && event.button.y <= 174)) // EXIT
+			{
+
+			}
+			else
+			{
+				SDL_SetRenderDrawColor(main_renderer, 0, 0, 0, 255);
+				SDL_BlitSurface(menu, NULL, tela, NULL);
+				SDL_UpdateWindowSurface(main_window);
+			}
+
+
+			switch (event.type)
+			{
+
+
+			case SDL_MOUSEBUTTONDOWN:
+				if (event.button.button == SDL_BUTTON_LEFT || event.button.button == SDL_BUTTON_RIGHT)
+				{
+					
+					if ((event.button.x >= 601 && event.button.y >= 69) && (event.button.x <= 688 && event.button.y <= 94)) // START
+					{
+						Menu = 0;
+						Play();
+					}
+					else if ((event.button.x >= 601 && event.button.y >= 149) && (event.button.x <= 679 && event.button.y <= 174)) // EXIT
+					{
+						Exit();
+					}
+				}
+				break;
+			}
+		}
+	}
+	
+    Exit();
+}
---
+++ Pong2/Pong/pong.h
@@ -0,0 +1,52 @@
+#ifndef PONG_H_
+#define PONG_H_
+#include "elite.h"
+
+typedef enum {FALSE, TRUE} bool;
+typedef char* string;
+const string title;
+SDL_Window *main_window;
+SDL_Renderer *main_renderer;
+SDL_Texture *Points[11];
+//Mix_Music *themeSong = NULL;
+SDL_Surface *menu;
+SDL_Surface *play;
+SDL_Surface* tela;
+
+typedef struct s_player
+{
+    SDL_Rect Player1, Player2;
+    SDL_Rect Bolinha;
+    SDL_Rect Score_Player1, Score_Player2;
+    float multiplierX, multiplierY;
+    int PointsPlayer1, PointsPlayer2;
+
+}player_t;
+
+typedef struct s_colors
+{
+    int R,G,B;
+    string color_name;
+    struct s_colors *next_color;
+}
+colors_t;
+
+void InitSDL2();
+void DrawCenterLine(colors_t* list);
+void Exit();
+void PointRegisterSound();
+void InitializePlayers(player_t *Game);
+void DrawPlayers(player_t *Game, colors_t* list);
+void funcaoBolinha(player_t *Game);
+void CheckCollisionLeft(player_t *Game);
+void CheckCollisionRight(player_t *Game);
+void CheckCollisionTop(player_t *Game);
+void CheckCollisionBottom(player_t *Game);
+void Free_List(colors_t *list);
+void Init_Score();
+void DrawScore(player_t *Game);
+colors_t *Create_Color(colors_t* list, string name, int Red, int Green, int Blue);
+colors_t *Get_Color(colors_t *list, string name);
+colors_t* Create_List(colors_t *list);
+
+#endif
